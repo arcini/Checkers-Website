bcf23bc682afcde33f8c7d18daa3c70526d5a34b
carter19d <33102806+carter19d@users.noreply.github.com>
2017-10-26 13:48:17 -0400

    started class and colored board

--- cini19aFinal.js
+++ cini19aFinal.js
@@ -1,7 +1,33 @@
 window.addEventListener("load", init);
 
 function init() {
+  class Piece {
+    constructor(type, player, meta=undefined) {
+      this.type = type;
+      this.player = player;
+      this.meta = meta;
+    }
+    getMoveSquares(board) {
+      if (this.type == ) {
 
+      }
+    }
+  }
+  boardElement = document.getElementsByClassName('divTableCell');
+  boardElArray = [];
+  pieceArray = [];
+  for (let x = 0; x < 8; x++) {
+    boardArray.push([]);
+    pieceArray.push([]);
+    for (let y = 0; y < 8; y++) {
+      boardElArray[x].push(boardElement[x*8+y]);
+      pieceArray.push(new Piece)
+      if ((x + y) % 2 == 1) {
+        boardElArray[x][y].style.backgroundColor = "black";
+      }
+      boardElArray[x][y].id = '(' + x + ',' + y + ')';
+    }
+  }
 
 
 
