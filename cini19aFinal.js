bac9ea32af0a738d7df6889699d64c3ec23947f4
carter19d <33102806+carter19d@users.noreply.github.com>
2017-10-30 15:16:46 -0400

    stuff

--- cini19aFinal.js
+++ cini19aFinal.js
@@ -1,20 +1,52 @@
 window.addEventListener("load", init);
 class Piece {
-  constructor(x, y, type, player, meta=undefined) {
+  constructor(x, y, type, player) {
     this.x = x;
     this.y = y;
     this.type = type;
     this.player = player;
-    this.meta = meta;
+  }
+  hypothetical(x, y) {
+    return new Piece(x, y, this.type, this.player);
   }
   getMoveSquares(board) {
     let move = [];
     let capture = [];
     if (this.player == 1 || this.type == 'king') {
-      if ()
+      if (board[this.x - 1][this.y - 1].type == 'none') {
+        move.push([this.x - 1, this.y - 1]);
+      } else if (board[this.x - 1][this.y - 1].player == 3-this.player
+                 && board[this.x - 2][this.y - 2].type == 'none') {
+        capture.push([this.x - 2, this.y - 2]);
+        nextCapture = this.hypothetical(this.x - 2, this.y - 2).getMoveSquares(board);
+        //STOPPED HERE
+      }
+      if (board[this.x - 1][this.y + 1].type == 'none') {
+        move.push([this.x - 1, this.y + 1]);
+      } else if (board[this.x - 1][this.y + 1].player == 3-this.player
+                 && board[this.x - 2][this.y + 2].type == 'none') {
+        capture.push([this.x - 2, this.y + 2]);
+      }
+    }
+    if (this.player == 1 || this.type == 'king') {
+      if (board[this.x - 1][this.y - 1].type == 'none') {
+        move.push([this.x - 1, this.y - 1]);
+      } else if (board[this.x - 1][this.y - 1].player == 3-this.player
+                 && board[this.x - 2][this.y - 2].type == 'none') {
+        capture.push([this.x - 2, this.y - 2]);
+      }
+      if (board[this.x - 1][this.y + 1].type == 'none') {
+        move.push([this.x - 1, this.y + 1]);
+      } else if (board[this.x - 1][this.y + 1].player == 3-this.player
+                 && board[this.x - 2][this.y + 2].type == 'none') {
+        capture.push([this.x - 2, this.y + 2]);
+      }
     }
     return [move, capture, special];
   }
+}
+function makeMove(x1, y1, x2, y2, type, board) {
+  var boardC = JSON.parse(JSON.stringify(board));
 }
 function init() {
   var boardElement = document.getElementsByClassName('divTableCell');
