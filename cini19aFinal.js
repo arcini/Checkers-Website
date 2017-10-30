eb214e24baadd7891e6709195b999223f7694238
Alex Cini <arcini8@gmail.com>
2017-10-30 17:20:29 -0400

    jesus christ my lord and savior please commit

--- cini19aFinal.js
+++ cini19aFinal.js
@@ -1,4 +1,7 @@
 window.addEventListener("load", init);
+function works(x,y) {
+  return (0<=x && x<=7 && 0<=y && y<=7);
+}
 class Piece {
   constructor(x, y, type, player) {
     this.x = x;
@@ -13,54 +16,62 @@ class Piece {
     let move = [];
     let capture = [];
     if (this.player == 1 || this.type == 'king') {
-      if (board[this.x - 1][this.y - 1].type == 'none') {
-        move.push([this.x - 1, this.y - 1]);
-      } else if (board[this.x - 1][this.y - 1].player == 3-this.player
-                 && board[this.x - 2][this.y - 2].type == 'none') {
-        capture.push([this.x - 2, this.y - 2]);
-        nextCapture = this.hypothetical(this.x - 2, this.y - 2).getMoveSquares(makeMove(this.x, this.y, this.x - 2, this.y - 2, 'capture', board))[1];
-        for (let i = 0; i < nextCapture.length; i++) {
-          capture.push([this.x - 2, this.y - 2].concat(nextCapture[i]));
-        }
+      if (works(this.x - 1, this.y - 1)) {
+        if (board[this.x - 1][this.y - 1].type == 'none') {
+          move.push([this.x - 1, this.y - 1]);
+        } /*else if (board[this.x - 1][this.y - 1].player == 3-this.player
+                   && board[this.x - 2][this.y - 2].type == 'none') {
+          capture.push([this.x - 2, this.y - 2]);
+          nextCapture = this.hypothetical(this.x - 2, this.y - 2).getMoveSquares(makeMove(this.x, this.y, this.x - 2, this.y - 2, 'capture', board))[1];
+          for (let i = 0; i < nextCapture.length; i++) {
+            capture.push([this.x - 2, this.y - 2].concat(nextCapture[i]));
+          }
+        }*/
       }
-      if (board[this.x - 1][this.y + 1].type == 'none') {
-        move.push([this.x - 1, this.y + 1]);
-      } else if (board[this.x - 1][this.y + 1].player == 3-this.player
-                 && board[this.x - 2][this.y + 2].type == 'none') {
-        capture.push([this.x - 2, this.y + 2]);
-        nextCapture = this.hypothetical(this.x - 2, this.y + 2).getMoveSquares(makeMove(this.x, this.y, this.x - 2, this.y + 2, 'capture', board))[1];
-        for (let i = 0; i < nextCapture.length; i++) {
-          capture.push([this.x - 2, this.y + 2].concat(nextCapture[i]));
-        }
+      if (works(this.x - 1, this.y + 1)) {
+        if (board[this.x - 1][this.y + 1].type == 'none') {
+          move.push([this.x - 1, this.y + 1]);
+        } /*else if (board[this.x - 1][this.y + 1].player == 3-this.player
+                   && board[this.x - 2][this.y + 2].type == 'none') {
+          capture.push([this.x - 2, this.y + 2]);
+          nextCapture = this.hypothetical(this.x - 2, this.y + 2).getMoveSquares(makeMove(this.x, this.y, this.x - 2, this.y + 2, 'capture', board))[1];
+          for (let i = 0; i < nextCapture.length; i++) {
+            capture.push([this.x - 2, this.y + 2].concat(nextCapture[i]));
+          }
+        }*/
       }
     }
-    if (this.player == 1 || this.type == 'king') {
-      if (board[this.x + 1][this.y - 1].type == 'none') {
-        move.push([this.x + 1, this.y - 1]);
-      } else if (board[this.x + 1][this.y - 1].player == 3-this.player
-                 && board[this.x + 2][this.y - 2].type == 'none') {
-        capture.push([this.x + 2, this.y - 2]);
-        nextCapture = this.hypothetical(this.x + 2, this.y - 2).getMoveSquares(makeMove(this.x, this.y, this.x + 2, this.y - 2, 'capture', board))[1];
-        for (let i = 0; i < nextCapture.length; i++) {
-          capture.push([this.x + 2, this.y - 2].concat(nextCapture[i]));
-        }
+    if (this.player == 2 || this.type == 'king') {
+      if (works(this.x + 1, this.y - 1)) {
+        if (board[this.x + 1][this.y - 1].type == 'none') {
+          move.push([this.x + 1, this.y - 1]);
+        } /*else if (board[this.x + 1][this.y - 1].player == 3-this.player
+                   && board[this.x + 2][this.y - 2].type == 'none') {
+          capture.push([this.x + 2, this.y - 2]);
+          nextCapture = this.hypothetical(this.x + 2, this.y - 2).getMoveSquares(makeMove(this.x, this.y, this.x + 2, this.y - 2, 'capture', board))[1];
+          for (let i = 0; i < nextCapture.length; i++) {
+            capture.push([this.x + 2, this.y - 2].concat(nextCapture[i]));
+          }
+        }*/
       }
-      if (board[this.x + 1][this.y + 1].type == 'none') {
-        move.push([this.x + 1, this.y + 1]);
-      } else if (board[this.x + 1][this.y + 1].player == 3-this.player
-                 && board[this.x + 2][this.y + 2].type == 'none') {
-        capture.push([this.x + 2, this.y + 2]);
-        nextCapture = this.hypothetical(this.x + 2, this.y + 2).getMoveSquares(makeMove(this.x, this.y, this.x + 2, this.y + 2, 'capture', board))[1];
-        for (let i = 0; i < nextCapture.length; i++) {
-          capture.push([this.x + 2, this.y + 2].concat(nextCapture[i]));
-        }
+      if (works(this.x + 1, this.y + 1)) {
+        if (board[this.x + 1][this.y + 1].type == 'none') {
+          move.push([this.x + 1, this.y + 1]);
+        } /*else if (board[this.x + 1][this.y + 1].player == 3-this.player
+                   && board[this.x + 2][this.y + 2].type == 'none') {
+          capture.push([this.x + 2, this.y + 2]);
+          nextCapture = this.hypothetical(this.x + 2, this.y + 2).getMoveSquares(makeMove(this.x, this.y, this.x + 2, this.y + 2, 'capture', board))[1];
+          for (let i = 0; i < nextCapture.length; i++) {
+            capture.push([this.x + 2, this.y + 2].concat(nextCapture[i]));
+          }
+        }*/
       }
     }
     return [move, capture];
   }
 }
 function makeMove(x1, y1, x2, y2, type, board) {
-  var boardC = JSON.parse(JSON.stringify(board));
+  var boardC = board.slice();
   boardC[x2][y2] = boardC[x1][y1].hypothetical(x2, y2);
   boardC[x1][y1] = new Piece(x1, y1, 'none', undefined);
   if (type == 'capture') {
@@ -84,6 +95,13 @@ function init() {
         boardElArray[x][y].style.backgroundColor = "black";
       }
       boardElArray[x][y].id = '(' + x + ',' + y + ')';
+      if ((x + y)%2 == 1 && x < 3) {
+        pieceArray[x][y] = new Piece(x, y, 'normal', 2);
+      } else if ((x + y)%2 == 1 && x >= 5) {
+        pieceArray[x][y] = new Piece(x, y, 'normal', 1);
+      } else {
+        pieceArray[x][y] = new Piece(x, y, 'none', undefined);
+      }
     }
   }
 
@@ -176,28 +194,32 @@ function init() {
 
 
 
-
-for (let x = 0; x < 8; x++) {
-  for (let y = 0; y < 8; y++) {
-    if ((x + y)%2 == 1 && x < 3) {
-      let yeet = document.createElement("img");
-      yeet.setAttribute("src", "redChecker.png");
-      yeet.style.width = "80%";
-      yeet.style.height = "80%";
-      boardElArray[x][y].appendChild(yeet);
-      pieceArray[x][y] = new Piece(x, y, 'normal', 1);
-    } else if ((x + y)%2 == 1 && x >= 5) {
-      let yeet = document.createElement("img");
-      yeet.setAttribute("src", "blueChecker.png");
-      yeet.style.width = "80%";
-      yeet.style.height = "80%";
-      boardElArray[x][y].appendChild(yeet);
-      pieceArray[x][y] = new Piece(x, y, 'normal', 2);
-    } else {
-      pieceArray[x][y] = new Piece(x, y, 'none', 0);
+function render() {
+  for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; y++) {
+      if ((x + y)%2 == 1 && x < 3) {
+        let yeet = document.createElement("img");
+        yeet.setAttribute("src", "redChecker.png");
+        yeet.style.width = "80%";
+        yeet.style.height = "80%";
+        while (boardElArray[x][y].hasChildNodes()) {
+          boardElArray[x][y].removeChild(boardElArray[x][y].lastChild);
+        }
+        boardElArray[x][y].appendChild(yeet);
+      } else if ((x + y)%2 == 1 && x >= 5) {
+        let yeet = document.createElement("img");
+        yeet.setAttribute("src", "blueChecker.png");
+        yeet.style.width = "80%";
+        yeet.style.height = "80%";
+        while (boardElArray[x][y].hasChildNodes()) {
+          boardElArray[x][y].removeChild(boardElArray[x][y].lastChild);
+        }
+        boardElArray[x][y].appendChild(yeet);
+      }
     }
   }
 }
+render();
 
 
 
@@ -252,26 +274,45 @@ for (let x = 0; x < 8; x++) {
 
 
 
-
-
+var isPlayer = 1;
 var isClicked = [false, 1, 1]; //index 0 tells whether a square is currently in the clicked state, indexes 1 and 2 give it's x and y position
 var lastBGC;
 function clickedSquare(x, y) {
-  //let squares = getMoveSquares(boardElArray); //[moved, capture, special]
-  if (isClicked[0]) {
-    boardElArray[isClicked[1]][isClicked[2]].style.backgroundColor = lastBGC;
+  for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; y++) {
+      if ((x + y) % 2 == 1) {
+        boardElArray[x][y].style.backgroundColor = "black";
+      }
+    }
   }
-  lastBGC = boardElArray[x][y].style.backgroundColor;
-  boardElArray[x][y].style.backgroundColor = "green";
-  isClicked = [true, x, y]; //memory for the state of the clicked square
+  if (pieceArray[x][y].player == isPlayer) {
+    if (isClicked[0]) {
+      boardElArray[isClicked[1]][isClicked[2]].style.backgroundColor = lastBGC;
+    }
+    let moveArray = pieceArray[x][y].getMoveSquares(pieceArray)[0];
+    for (i = 0; i < moveArray.length; i++) {
+      boardElArray[moveArray[i][0]][moveArray[i][1]].style.backgroundColor = "yellow";
+      boardElArray[moveArray[i][0]][moveArray[i][1]].removeEventListener("click", function(e){
+        clickedSquare(x, y);
+      });
+      boardElArray[moveArray[i][0]][moveArray[i][1]].addEventListener("click", function(e) {
+        pieceArray = makeMove(x, y, moveArray[i-1][0], moveArray[i-1][1], 'move', pieceArray);
+        isPlayer = 3 - isPlayer;
+        render();
+      });
+    }
+    lastBGC = boardElArray[x][y].style.backgroundColor;
+    boardElArray[x][y].style.backgroundColor = "green";
+    isClicked = [true, x, y]; //memory for the state of the clicked square
+  }
 }
-
 for (let x = 0; x < boardElArray.length; x++) {
   for (let y = 0; y < boardElArray[x].length; y++) {
     let d = boardElArray[x][y];
-    d.addEventListener("click", function(e) {
-      clickedSquare(x, y);
-    });
+    function tempFunc() {
+      clickedSquare(x,y);
+    }
+    d.addEventListener("click", tempFunc);
   }
 }
 
